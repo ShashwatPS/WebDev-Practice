d47e29a178476d4324e6c7900320a7de78facabc
Shashwat Singh <shashwat123student@gmail.com>
2023-08-11 09:54:23 +0530

    Added a Landing Page

--- Random_Quote_Generator/src/App.jsx
+++ Random_Quote_Generator/src/App.jsx
@@ -1,8 +1,34 @@
+import {useEffect} from "react";
+import {RecoilRoot, useRecoilState, useRecoilValue} from "recoil";
+import axios from "axios";
+import {setquoteState} from "./Selectors/quote.js";
+import {quoteState} from "./atoms/quotegen.js";
+import {Router} from "express";
+import LandingPage from "./Landing-Page.js";
+import {Route, Routes} from "react-router-dom";
+
 function App() {
   return (
-    <>
-    </>
+    <RecoilRoot>
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+      </Routes>
+    </RecoilRoot>
   )
 }
 
+function Init(){
+  const setquote = useRecoilValue(quoteState);
+  const quote = useRecoilState(setquoteState);
+
+  useEffect(()=>{
+    axios.get("https://api.quotable.io/quotes/random",{
+      method: "GET",
+    }).then(res=>{
+      setquote(res.data);
+    })
+  },[])
+
+  return <></>;
+}
 export default App
--- Random_Quote_Generator/src/atoms/quotegen.js
+++ Random_Quote_Generator/src/atoms/quotegen.js
@@ -3,6 +3,11 @@ import {atom} from "recoil";
 export const quoteState = atom({
     key: 'quoteState',
     default: {
-
+        _id: null,
+        content: null,
+        author: null,
+        authorSlug: null,
+        length: 0,
+        tags: [],
     }
 })
