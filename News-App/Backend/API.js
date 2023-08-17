1bc4843164435f0495ffd90c524c3c7af4fd8770
Shashwat Singh <shashwat123student@gmail.com>
2023-08-17 17:41:00 +0530

    Finished Signup Route

--- News-App/Backend/API.js
+++ News-App/Backend/API.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const {SECRET} = require("./middleware/auth.js");
+const {sign} = require("jsonwebtoken");
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -13,7 +15,22 @@ const adminSchema = mongoose.Schema({
     password: String,
 });
 
-app.post('/signup', (req,res)=>{
+const Admin = mongoose.model("Admin", adminSchema);
+
+
+
+app.post('/signup', async (req,res)=>{
     const {username,password} = req.body;
-    
+    const admin = await Admin.findOne({username});
+    if(admin){
+        res.status(403).json({message: 'Admin already exists'});
+    }
+    else{
+        const obj = {username:username, password: password};
+        const newAdmin = new Admin(obj);
+        await newAdmin.save();
+
+        const token = sign({username, role:'admin'}, SECRET, {expires_in: '1h'});
+        res.json({message: 'Admin created successfully', token});
+    }
 })
