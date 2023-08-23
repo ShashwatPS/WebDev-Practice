5ad4c2318b0e7ace49a1a10700f2a51e8426ec3c
Shashwat Singh <shashwat123student@gmail.com>
2023-08-23 17:52:32 +0530

    Fixing the File Structure

---
+++ News-App/src/Selectors/Atoms/newsAtom.js
@@ -0,0 +1,20 @@
+import atom from "recoil";
+
+export const newsAtom = atom({
+
+    status: "",
+    totalResults: 0,
+    articles: [
+        {
+            source: {
+                id: null,
+                name: "",
+            },  author: "",
+            title: "",
+            description: "",
+            url: "",
+            urlToImage: "",
+            publishedAt: "",
+            content: "",
+        }]
+})
