f054822616c299fdb03caabb7eda018d87ac801d
Shashwat Singh <shashwat123student@gmail.com>
2023-08-27 21:06:50 +0530

    Fixinf the AppBar behaviour on signing in and signing out

--- News-App/src/AppBar.jsx
+++ News-App/src/AppBar.jsx
@@ -1,7 +1,60 @@
 import {AppBar, Box, Drawer, Typography} from "@mui/material";
 import SideNav from "./SideNav.jsx"
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Button from "@mui/material/Button";
 
 function StyleBar(){
+    const [userEmail,setEmail]=useState(null);
+
+    useEffect(()=>{
+        axios.get("http://localhost:3000/me").then(response=>{
+            setEmail(response.data);
+        }).catch(error=>{
+            console.error("Error fetching data:", error);
+        })
+    },[])
+
+    if(userEmail){
+        return(
+            <div>
+                <Box sx={{ flexGrow: 1 }}>
+                    <AppBar className={"appBar"} position="static" sx={{
+                        bgcolor: "#FF6347",
+                        height: "30px",
+                    }} >
+                        <div style={{
+                            paddingTop: 5,
+                            paddingLeft: 5,
+                            display: "flex",
+                            justifyContent: "space-between",
+                        }}>
+                            <Typography variant="h7">
+                                NEWS APP
+                            </Typography>
+                            <div>
+                            <Typography>
+                                {userEmail}
+                            </Typography>
+                                <Button
+                                    color="secondary"
+                                    disabled={false}
+                                    size="medium"
+                                    variant="filled"
+                                    onClick={()=>{
+                                        localStorage.setItem("token",null);
+                                        window.location= "/"
+                                    }}
+                                >Logout</Button>
+                            <SideNav></SideNav>
+                            </div>
+                        </div>
+                    </AppBar>
+                </Box>
+            </div>
+        )
+    }
+    else{
     return(
         <div>
             <Box sx={{ flexGrow: 1 }}>
@@ -24,6 +77,5 @@ function StyleBar(){
             </Box>
         </div>
     )
-}
-
+}}
 export default StyleBar;
