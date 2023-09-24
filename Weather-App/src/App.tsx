f2c09c7842e37365140570ed11d8c21174207aaf
Shashwat Singh <shashwat123student@gmail.com>
2023-09-24 17:26:40 +0530

    Started Weather-App

---
+++ Weather-App/src/App.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import {Navbar} from "./Components/Navbar.tsx";
+function App() {
+   return(
+       <div>
+           <Navbar/>
+           <Routes>
+               <Route></Route>
+           </Routes>
+       </div>
+   )
+}
+
+export default App
